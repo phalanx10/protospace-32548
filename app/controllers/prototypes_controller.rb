4758f82b96277a028a500a5732f56382bfbda183
phalanx10 <phalanx.1379@gmail.com>
2020-12-07 14:09:43 +0900

    ログインユーザーのみアクションを許可

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
     @prototype = Prototype.all
   end
@@ -19,6 +21,12 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
